c138510e8a2d868db67eef6a121248ec5abb403c
Tim12er <tim.zwoelfer@edu.fh-joanneum.at>
2024-12-26 16:00:16 +0100

    Changed the Automatic Port Selection

--- Main/main.m
+++ Main/main.m
@@ -3,13 +3,28 @@ disp("Starting the Floating Ball...")
 
 
 if exist('baudRate', 'var') == 0
-    clear
-    PortAsString = serialportlist;
-    PortAvailable = char(extractBetween(PortAsString, 1, 4));
-    baudRate = 19200;
-    timeout = 10;
-    arduino = serialport(PortAvailable, baudRate, 'Timeout', timeout);
-    configureTerminator(arduino,59,"LF");
+  clear;
+  PortList = serialportlist;
+
+  baudRate = 19200;
+  timeout = 10;
+  ArduinoPort = "";
+  for i = 1:length(PortList)
+      try
+          port = char(PortList(i)); % current Port
+          arduino = serialport(port, baudRate, 'Timeout', timeout);
+      catch
+          % No error handling, try next Port
+      end
+  end
+
+  % clear;
+  %   PortAsString = serialportlist;
+  %   PortAvailable = char(extractBetween(PortAsString, 1, 4));
+  %   baudRate = 19200;
+  %   timeout = 10;
+  %   arduino = serialport('COM5', baudRate, 'Timeout', timeout);
+  %   configureTerminator(arduino,59,"LF");
 end
 
 guiName = 'Ball and Motor Control';
